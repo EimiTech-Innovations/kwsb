7dc2fe4ce4c3ee8ff8dfa476addbae348f9e6f15
MTouthang <huebart16@gmail.com>
2025-04-04 01:13:55 +0530

    fix: hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,12 +1,16 @@
-import heroImage from "../../assets/Images/heroImage.png"
+import heroImage from "../../assets/Images/heroImage.jpeg"
 
 const Hero = () => {
     return (
-        <img
-            className="h-96 w-full object-cover"
-            src={heroImage}
-            alt="KWS Bangalore church"
-        />
+        <>
+            <div className="h-96 absolute w-full bg-gradient-to-b from-transparent via-transparent to-black/70"></div>
+            <img
+                className="h-96 w-full object-cover"
+                src={heroImage}
+                alt="KWS Bangalore church"
+            />
+        </ >
+
 
     );
 };
