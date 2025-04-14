f7f41d53f19a28614c8365c75f066b8c283271ad
MTouthang <huebart16@gmail.com>
2025-04-15 00:43:43 +0530

    feat: event data

---
+++ src/data/events.ts
@@ -0,0 +1,94 @@
+import { Event } from "../types/event";
+
+export const events: Event[] = [
+  {
+    id: "event2025001",
+    eventName: "Palm Sunday",
+    eventDate: new Date("2025-04-13"),
+  },
+  {
+    id: "event2025002",
+    eventName: "Good Friday",
+    eventDate: new Date("2025-04-18"),
+  },
+  {
+    id: "event2025003",
+    eventName: "Easter Sunday",
+    eventDate: new Date("2025-04-20"),
+  },
+  {
+    id: "event2025004",
+    eventName: "Mother's Day",
+    eventDate: new Date("2025-05-11"),
+  },
+  {
+    id: "event2025005",
+    eventName: "1. Pentecost Day & 2. Children's Day",
+    eventDate: new Date("2025-06-08"),
+  },
+  {
+    id: "event2025006",
+    eventName: "Father's Day",
+    eventDate: new Date("2025-06-15"),
+  },
+  {
+    id: "event2025007",
+    eventName: "Half Yearly Assembly",
+    eventDate: new Date("2025-07-13"),
+  },
+  {
+    id: "event2025008",
+    eventName: "Anthah Lop",
+    eventDate: new Date("2025-08-10"),
+  },
+  {
+    id: "event2025009",
+    eventName: "Pastor Appreciation Day",
+    eventDate: new Date("2025-10-12"),
+  },
+  {
+    id: "event2025010",
+    eventName: "Youth Day Celebration",
+    eventDate: new Date("2025-10-14"),
+  },
+  {
+    id: "event2025011",
+    eventName: "Gospel Adontes cum Advent Christmas",
+    eventDate: new Date("2025-11-30"),
+  },
+  {
+    id: "event2025012",
+    eventName: "Christmas Eve",
+    eventDate: new Date("2025-12-24"),
+  },
+  {
+    id: "event2025013",
+    eventName: "Christmas Day",
+    eventDate: new Date("2025-12-25"),
+  },
+  {
+    id: "event2025014",
+    eventName: "New Year's Eve",
+    eventDate: new Date("2025-12-31"),
+  },
+  {
+    id: "event2025015",
+    eventName: "New Year's Day",
+    eventDate: new Date("2025-01-01"),
+  },
+  {
+    id: "event2025016",
+    eventName: "Annual General Assembly",
+    eventDate: new Date("2025-01-11"),
+  },
+  {
+    id: "event2025017",
+    eventName: "General Election / Nomination",
+    eventDate: new Date("2025-01-18"),
+  },
+  {
+    id: "event2025018",
+    eventName: "Dedication of New Executive Committee 2026",
+    eventDate: new Date("2025-01-25"),
+  },
+];
---
+++ src/types/event.d.ts
@@ -0,0 +1,5 @@
+export type Event = {
+  id: string;
+  eventName: string;
+  eventDate: Date;
+};
